93a569d0c956e677336d7d60817f30bcff1333fb
Joseph O'Dell <josephodell@outlook.com>
2018-12-10 11:33:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size < 3
+    array.join(" and ")
+  else
+    last = array.pop
+    string = array.join(", ")
+    string = string + ", and #{last}"
+  end
 end
