12c4104c38aa0d43a5ed6b9afeca8cf7872d954c
jchaselubitz <jchaselubitz@gmail.com>
2018-12-12 16:44:03 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  printThis = ""
+  if array.size <= 2
+    printThis = array.join(" and ")
+  elsif array.size >= 3
+    endArray = []
+    2.times do
+      endArray.unshift(array.delete_at(-1))
+    end
+    # puts endArray
+    printEnd = endArray.join(", and ")
+    printBegin = array.join(", ")
+    printThis = printBegin + ", " + printEnd
+  end
+  #puts printThis
+  printThis
 end
+
+array = ["kiwi", "durian", "starfruit", "mangos"]
+oxford_comma(array)
