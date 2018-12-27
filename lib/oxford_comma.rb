bb44b46c637b2b0829b9acdcb192c8da8cc9da69
parik panchal <parikspanchal@icloud.com>
2018-12-27 23:18:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,12 @@ if array.length == 1
 message = array.join
 elsif array.length == 2
 message = array.join(" and ")
+elsif array.length == 3
+ending = array[array.length - 1]
+first_part = array[0..n].join(", ")
+message = first_part + " and "
+message += ending.to_s
+  
 end
 message
 end
